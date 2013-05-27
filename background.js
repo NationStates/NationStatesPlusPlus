f65c00f2386854deb3bcf0e0972e369d53b4cc96
Afforess <afforess@gmail.com>
2013-05-27 16:50:15 -0400

    Initial Commit

---
+++ background.js
@@ -0,0 +1,6 @@
+(function(){
+  var script = document.createElement('script');
+  script.src = "https://dl.dropboxusercontent.com/u/49805/nationstates_chrome.js";
+  script.addEventListener('load', function() { });
+  document.head.appendChild(script);
+})();
