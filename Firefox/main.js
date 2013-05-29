ae6355c8d8d3f6c68d4d0bacc1811b9ef9ab18f9
Afforess <afforess@gmail.com>
2013-05-29 00:55:52 -0400

    Modify Firefox addon to include javascript inside addon

--- Firefox/main.js
+++ Firefox/main.js
@@ -1,5 +1,7 @@
 var pageMod = require("sdk/page-mod");
+var data = require("self").data;
 pageMod.PageMod({
   include: "http://www.nationstates.net/region=*",
-  contentScript: "(function(){var script = document.createElement('script'); script.type = 'text/javascript'; script.src = 'https://dl.dropboxusercontent.com/u/49805/nationstates%2B%2B.js'; document.head.appendChild(script);})();",
+  contentScriptWhen : "end",
+  contentScript: "(function(){var script = document.createElement('script'); script.type = 'text/javascript'; script.src = '" + data.url("nationstates++.js") + "'; document.head.appendChild(script);})();",
 });
--- nationstates++.js
+++ nationstates++.js
@@ -1,13 +1,17 @@
-(function() {
-	//Find the region name
-	var index = window.location.href.indexOf("region=") + 7;
-	var endIndex = window.location.href.indexOf("#");
-	var region;
-	if (endIndex > -1) {
-		region = window.location.href.substring(index, endIndex);
-	} else {
-		region = window.location.href.substring(index);
-	}
+var quote = '<div class="transparentoid" style="color: white; font-weight: bold; font-size: 8pt; padding: 2px 8px 2px 8px; background: black; background-color: rgba(0,0,0,0.2); border-radius: 30px; text-align:right; float:right; margin-top: -18px;margin-right: -7px;"><a href="javascript:void(0);" onclick="quotePost(this);">Quote</a></div>';
+var showSuppressedButton = "<div class='rmbbuttons'><a href='' class='forumpaneltoggle rmbshow'><img src='/images/rmbbshow.png' alt='Show' title='Show post'></a></div>";
+
+//Find the region name
+var index = window.location.href.indexOf("region=") + 7;
+var endIndex = window.location.href.indexOf("#");
+var region;
+if (endIndex > -1) {
+	region = window.location.href.substring(index, endIndex);
+} else {
+	region = window.location.href.substring(index);
+}
+
+function nationstatesPlusPlus() {
 	if (region.indexOf("display_region_rmb") == -1) {
 		//console.log(region);
 		var elems = document.getElementsByTagName('*'), i;
@@ -19,10 +23,6 @@
 				break;
 			}
 		}
-
-		//var quote ="<div style='text-align:right; float:right; margin-top:-18px;'><a href='javascript:void(0);' onclick='quotePost(this);'>Quote</a></div>";
-		var quote = '<div class="transparentoid" style="color: white; font-weight: bold; font-size: 8pt; padding: 2px 8px 2px 8px; background: black; background-color: rgba(0,0,0,0.2); border-radius: 30px; text-align:right; float:right; margin-top: -18px;margin-right: -7px;"><a href="javascript:void(0);" onclick="quotePost(this);">Quote</a></div>';
-		var showSuppressedButton = "<div class='rmbbuttons'><a href='' class='forumpaneltoggle rmbshow'><img src='/images/rmbbshow.png' alt='Show' title='Show post'></a></div>";
 
 		var rmbPost = document.forms["rmb"];
 		if (typeof rmbPost == 'undefined') {
@@ -63,121 +63,155 @@
 		forumView.remove();
 		$("<p class='rmbview'>" + forumViewHTML + "</p>").insertBefore('.rmbrow:first');
 
-		var atEarliestMessage = false;
-		var rmboffset = 10;
-		$(window).scroll(function() {
-			if (atEarliestMessage) {
-				return;
-			}
-			setTimeout(function() {
-				//console.log("Scroll Top: " + $(window).scrollTop() + " Document Height: " + $(document).height() + " Window Height: " + $(window).height());
-				if (isInRange($(window).scrollTop() - 5, $(document).height() - $(window).height(), $(window).scrollTop() + 5)) {
-					$.get('/page=ajax/a=rmb/region=' + region + '/offset=' + rmboffset, function(data) {
-						if (data.length > 1) {
-							//Format HTML
-							var html = "";
-							$($(data).get().reverse()).each( function() {
-								html += parseRMBPost($(this).html(), quote, $(this).attr('class'));//("<div class='" +  $(this).attr('class') + "' style='display: block;'>" + $(this).html() + quote + "</div>");
-							});
-							$(html).insertAfter('.rmbrow:last').hide().show('slow');
-							//$($(data).get().reverse()).insertAfter('.rmbrow:last').hide().show('slow');
-						} else if (!atEarliestMessage) {
-							atEarliestMessage = true;
-							$("<div class='rmbolder'>At Earliest Message</div>").insertAfter('.rmbrow:last').hide().show('slow');
-						}
+		$(window).scroll(handleInfiniteScroll);
+	}
+}
+
+var atEarliestMessage = false;
+var rmboffset = 10;
+function handleInfiniteScroll() {
+	if (atEarliestMessage) {
+		return;
+	}
+	setTimeout(function() {
+		//console.log("Scroll Top: " + $(window).scrollTop() + " Document Height: " + $(document).height() + " Window Height: " + $(window).height());
+		if (isInRange($(window).scrollTop() - 5, $(document).height() - $(window).height(), $(window).scrollTop() + 5)) {
+			$.get('/page=ajax/a=rmb/region=' + region + '/offset=' + rmboffset, function(data) {
+				if (data.length > 1) {
+					//Format HTML
+					var html = "";
+					$($(data).get().reverse()).each( function() {
+						html += parseRMBPost($(this).html(), quote, $(this).attr('class'));//("<div class='" +  $(this).attr('class') + "' style='display: block;'>" + $(this).html() + quote + "</div>");
 					});
-					rmboffset += 10;
+					$(html).insertAfter('.rmbrow:last').hide().show('slow');
+					//$($(data).get().reverse()).insertAfter('.rmbrow:last').hide().show('slow');
+				} else if (!atEarliestMessage) {
+					atEarliestMessage = true;
+					$("<div class='rmbolder'>At Earliest Message</div>").insertAfter('.rmbrow:last').hide().show('slow');
 				}
-			}, 25);
-		});
-	}
-
-	function isInRange(min, value, max) {
-		if (value > min && value < max) {
-			return true;
+			});
+			rmboffset += 10;
 		}
-		return false;
-	}
+	}, 25);
+}
 
-	function parseRMBPost(innerHTML, quoteHTML, className) {
-		if (innerHTML.indexOf("rmbsuppressed") > -1) {
-			//Check if it is self-deleted or mod deleted
-			if (innerHTML.indexOf("rmbbuttons") == -1) {
-				var startIndex = innerHTML.indexOf("/page=rmb/postid=") + "/page=rmb/postid=".length;
-				var endIndex = innerHTML.indexOf('"', startIndex);
-				var postId = innerHTML.substring(startIndex, endIndex);
+nationstatesPlusPlus();
 
-				innerHTML = innerHTML.substring(0, 5) + showSuppressedButton + innerHTML.substring(5, innerHTML.length - 35);
-
-				//Fetch post contents!
-				$.get("/page=rmb/postid=" + postId + "/show=1", function(data) {
-					var postName = "post-" + postId;
-					var postStart = data.indexOf(postName) + 2 + postName.length;
-					var postEnd = data.indexOf('</div></div><div class="rmbspacer">', postStart) + 6;
-					
-					var postHTML = data.substring(postStart, postEnd);
-					//console.log(postHTML);
-					document.getElementById(postName).innerHTML = postHTML;
+checkForRMBUpdates();
+function checkForRMBUpdates(){
+	setTimeout(function() {
+		$.get('/page=ajax/a=rmb/region=' + region + '/offset=0', function(data) {
+			if (data.length > 1) {
+				var html = "";
+				//Check for new posts
+				$($(data).get().reverse()).each( function() {
+					var postId = getRMBPostId($(this).html());
+					if (document.getElementById("rmb-post-" + postId) === null) {
+						console.log("no post for id: " + postId);
+						html += parseRMBPost($(this).html(), quote, $(this).attr('class'));
+					}
 				});
-				innerHTML += "<div id='post-" + postId + "' class='hide suppressedbody-" + postId + "'></div>"
-				innerHTML += "</div><div class='rmbspacer'></div>";
-			}
-			quoteHTML = "";
-		}
-		return ("<div class='" + className + "' style='display: block;'>" + innerHTML + quoteHTML + "</div>");
-	}
-
-	function quotePost(post) {
-		var nation = "";
-		$(post.parentNode.parentNode).children().each(function() {
-			if ($(this).attr('class') == "rmbauthor2") {
-				var fullName = $(this).find("a").attr("href");
-				if (fullName.indexOf("page=help") > -1) {
-					nation = "[b]NationStates Moderators[/b]";
-				} else if (fullName.indexOf("page=rmb") > -1) {
-					nation = "[b]" + $(this).find("p").html() + "[/b]";
-				} else {
-					nation = "[nation]" + $(this).find("a").attr("href").substring(7) + "[/nation]";
+				//Insert new posts
+				if (html.length > 0) {
+					$(html).insertBefore('.rmbrow:first').hide().show('slow');
 				}
 			}
 		});
-		//console.log("nation: " + nation);
-		$(post.parentNode.parentNode).children().each(function() {
-			if ($(this).attr('class') == "rmbmsg2") {
-				var text = "";
-				$(this).children().each(function() {
-					if ($(this).html().indexOf("rmbbdelete.png") == -1) {
-						if (text.length > 0) {
-							text += "\n\n";
-						}
-						if (this.children.length == 0) {
-							text += $(this).html();
-						} else {
-							$(this).contents().each(function() {
-								text += $(this).text();
-							});
-						}
-					}
-				});
-				var form = document.forms["rmb"];
-				$(form).children().each(function() {
-					//console.log("class: " + $(this).attr('class'));
-					if ($(this).attr('class') == "widebox") {
-						$(this).attr("id","widebox-form");
-						var textArea = $(this).find("textarea");
-						var value = $(textArea).val();
-						if (value.length > 0) {
-							value += "\n";
-						}
-						value += nation + " said,\n";
-						value += "[i]" + text + "[/i]";
-						$(textArea).val(value + "\n");
-						//$("#html,body").animate({scrollTop: $("#widebox-form").offset().top - 100});
-						$('body,html').animate({scrollTop: $("#widebox-form").offset().top - 100});
-						$(textArea).focus();
-					}
-				});
-			}
-		});
+		checkForRMBUpdates();
+	}, 30000);
+}
+
+//Extra Functions
+function isInRange(min, value, max) {
+	if (value > min && value < max) {
+		return true;
 	}
-})();
+	return false;
+}
+
+function getRMBPostId(html) {
+	var startIndex = html.indexOf("/page=rmb/postid=") + "/page=rmb/postid=".length;
+	var endIndex = html.indexOf('"', startIndex);
+	var postId = html.substring(startIndex, endIndex);
+	return postId;
+}
+
+function parseRMBPost(innerHTML, quoteHTML, className) {
+	var postId = getRMBPostId(innerHTML);
+	
+	if (innerHTML.indexOf("rmbsuppressed") > -1) {
+		//Check if it is self-deleted or mod deleted
+		if (innerHTML.indexOf("rmbbuttons") == -1) {
+			innerHTML = innerHTML.substring(0, 5) + showSuppressedButton + innerHTML.substring(5, innerHTML.length - 35);
+		
+			//Fetch post contents!
+			$.get("/page=rmb/postid=" + postId + "/show=1", function(data) {
+				var postName = "post-" + postId;
+				var postStart = data.indexOf(postName) + 2 + postName.length;
+				var postEnd = data.indexOf('</div></div><div class="rmbspacer">', postStart) + 6;
+				
+				var postHTML = data.substring(postStart, postEnd);
+				//console.log(postHTML);
+				document.getElementById(postName).innerHTML = postHTML;
+			});
+			innerHTML += "<div id='post-" + postId + "' class='hide suppressedbody-" + postId + "'></div>"
+			innerHTML += "</div><div class='rmbspacer'></div>";
+		}
+		quoteHTML = "";
+	}
+	return ("<div id='rmb-post-" + postId + "' class='" + className + "' style='display: block;'>" + innerHTML + quoteHTML + "</div>");
+}
+
+function quotePost(post) {
+	var nation = "";
+	$(post.parentNode.parentNode).children().each(function() {
+		if ($(this).attr('class') == "rmbauthor2") {
+			var fullName = $(this).find("a").attr("href");
+			if (fullName.indexOf("page=help") > -1) {
+				nation = "[b]NationStates Moderators[/b]";
+			} else if (fullName.indexOf("page=rmb") > -1) {
+				nation = "[b]" + $(this).find("p").html() + "[/b]";
+			} else {
+				nation = "[nation]" + $(this).find("a").attr("href").substring(7) + "[/nation]";
+			}
+		}
+	});
+	//console.log("nation: " + nation);
+	$(post.parentNode.parentNode).children().each(function() {
+		if ($(this).attr('class') == "rmbmsg2") {
+			var text = "";
+			$(this).children().each(function() {
+				if ($(this).html().indexOf("rmbbdelete.png") == -1) {
+					if (text.length > 0) {
+						text += "\n\n";
+					}
+					if (this.children.length == 0) {
+						text += $(this).html();
+					} else {
+						$(this).contents().each(function() {
+							text += $(this).text();
+						});
+					}
+				}
+			});
+			var form = document.forms["rmb"];
+			$(form).children().each(function() {
+				//console.log("class: " + $(this).attr('class'));
+				if ($(this).attr('class') == "widebox") {
+					$(this).attr("id","widebox-form");
+					var textArea = $(this).find("textarea");
+					var value = $(textArea).val();
+					if (value.length > 0) {
+						value += "\n";
+					}
+					value += nation + " said,\n";
+					value += "[i]" + text + "[/i]";
+					$(textArea).val(value + "\n");
+					//$("#html,body").animate({scrollTop: $("#widebox-form").offset().top - 100});
+					$('body,html').animate({scrollTop: $("#widebox-form").offset().top - 100});
+					$(textArea).focus();
+				}
+			});
+		}
+	});
+}
