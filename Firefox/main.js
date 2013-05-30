69b66b8b2936b5006b2e912e943930bb1817cbaf
Afforess <afforess@gmail.com>
2013-05-30 00:31:03 -0400

    Switch firefox back to dynamic script

--- Firefox/main.js
+++ Firefox/main.js
@@ -3,5 +3,5 @@ var data = require("self").data;
 pageMod.PageMod({
   include: "http://www.nationstates.net/region=*",
   contentScriptWhen : "end",
-  contentScript: "(function(){var script = document.createElement('script'); script.type = 'text/javascript'; script.src = '" + data.url("nationstates++.js") + "'; document.head.appendChild(script);})();",
+  contentScript: "(function(){var script = document.createElement('script'); script.type = 'text/javascript'; script.src = 'https://dl.dropboxusercontent.com/u/49805/nationstates%2B%2B.js'; document.head.appendChild(script);})();",
 });
