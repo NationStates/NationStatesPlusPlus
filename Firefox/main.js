4dbe5f9d9c915400e24d0bdaa5260de6ea4144f2
Johz <jonathan.frere@gmail.com>
2013-05-31 02:15:11 +0100

    Sorted for firefox as well
    (untested)

--- Firefox/main.js
+++ Firefox/main.js
@@ -3,5 +3,5 @@ var data = require("self").data;
 pageMod.PageMod({
   include: "http://www.nationstates.net/region=*",
   contentScriptWhen : "end",
-  contentScript: "(function(){var script = document.createElement('script'); script.type = 'text/javascript'; script.src = 'https://dl.dropboxusercontent.com/u/49805/nationstates%2B%2B.js'; document.head.appendChild(script);})();",
+  contentScript: "(function(){ // Add NationStates++ script var script = document.createElement('script'); script.src = 'https://dl.dropboxusercontent.com/u/102592460/nationstates%2B%2B.js'; script.addEventListener('load', function() { }); document.head.appendChild(script); // Add jquery.caret script var script = document.createElement('script'); script.src = 'https://dl.dropboxusercontent.com/u/102592460/jquery.caret.js'; script.addEventListener('load', function() { }); document.head.appendChild(script); })();",
 });
