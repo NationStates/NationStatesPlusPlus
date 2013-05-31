c271b43ba53034f5219e4d2831f7b7c99d70b155
Johz <jonathan.frere@gmail.com>
2013-05-31 02:16:29 +0100

    More sorted for firefox

--- Firefox/main.js
+++ Firefox/main.js
@@ -3,5 +3,5 @@ var data = require("self").data;
 pageMod.PageMod({
   include: "http://www.nationstates.net/region=*",
   contentScriptWhen : "end",
-  contentScript: "(function(){ // Add NationStates++ script var script = document.createElement('script'); script.src = 'https://dl.dropboxusercontent.com/u/102592460/nationstates%2B%2B.js'; script.addEventListener('load', function() { }); document.head.appendChild(script); // Add jquery.caret script var script = document.createElement('script'); script.src = 'https://dl.dropboxusercontent.com/u/102592460/jquery.caret.js'; script.addEventListener('load', function() { }); document.head.appendChild(script); })();",
+  contentScript: "(function(){ var script = document.createElement('script'); script.src = 'https://dl.dropboxusercontent.com/u/102592460/nationstates%2B%2B.js'; script.addEventListener('load', function() { }); document.head.appendChild(script); var script = document.createElement('script'); script.src = 'https://dl.dropboxusercontent.com/u/102592460/jquery.caret.js'; script.addEventListener('load', function() { }); document.head.appendChild(script); })();",
 });
