7e1aeef70af34879c22e7ded65b1d0138ea0eeaa
Afforess <afforess@gmail.com>
2013-11-14 17:22:25 -0500

    Sync settings for first use

--- Extension/js/settings.js
+++ Extension/js/settings.js
@@ -103,6 +103,8 @@
 			
 			var api = getSettings();
 			api.pushUpdate();
+		} else if (getSettings().last_update == 0) {
+			getSettings().update(function() {console.log("Updating settings for first use")});
 		}
 	}
 	if (getVisiblePage() == "blank" && window.location.href.indexOf("ns_settings") != -1) {
