e90af8a58e8a621202f6167a6f3baa5d4bc753b8
Afforess <afforess@gmail.com>
2014-01-26 13:27:38 -0500

    Add setting for automatically hiding flag

--- Extension/js/nationstates.js
+++ Extension/js/nationstates.js
@@ -34,18 +34,20 @@
 		});
 
 		//Check for small screen heights
-		var minHeight = $("#panel").css("min-height");
-		$("#panel").css("min-height", "0px");
-		if ($("#panel").height() - 50 > $(window).height() || ($("#content").length == 0 && userSettings.isEnabled("small_screen_height", false))) {
-			$("#panel_flag").hide();
-			//Use this as a setting to sync with forumside
-			if (!userSettings.isEnabled("small_screen_height", false)) {
-				userSettings.setValue("small_screen_height", true);
+		if (userSettings.isEnabled("automatically_hide_flag", false)) {
+			var minHeight = $("#panel").css("min-height");
+			$("#panel").css("min-height", "0px");
+			if ($("#panel").height() - 50 > $(window).height() || ($("#content").length == 0 && userSettings.isEnabled("small_screen_height", false))) {
+				$("#panel_flag").hide();
+				//Use this as a setting to sync with forumside
+				if (!userSettings.isEnabled("small_screen_height", false)) {
+					userSettings.setValue("small_screen_height", true);
+				}
+			} else if (userSettings.isEnabled("small_screen_height", false)) {
+				userSettings.setValue("small_screen_height", false);
 			}
-		} else if (userSettings.isEnabled("small_screen_height", false)) {
-			userSettings.setValue("small_screen_height", false);
+			$("#panel").css("min-height", minHeight);
 		}
-		$("#panel").css("min-height", minHeight);
 	}
 
 	if ($(".menu").find("a[href='page=dilemmas']").html().match(/[0-9]+/) != null && getUserData().getValue("dismiss_all", false)) {
