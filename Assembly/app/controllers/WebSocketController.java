33c640c0db5b34ab60c7c882abab1176fa398e0d
Afforess <afforess@gmail.com>
2014-11-22 17:34:52 -0500

    document websocket entrypoints

--- Assembly/app/controllers/WebSocketController.java
+++ Assembly/app/controllers/WebSocketController.java
@@ -19,23 +19,63 @@ public class WebSocketController extends DatabaseController {
 		super(access, config);
 	}
 
+	/**
+	 * Opens a websocket for the given user nation and their current home region.
+	 * 
+	 * @param user nation that is opening the websocket.
+	 * @param userRegion name of the region the user currently resides in.
+	 * @param reconnect whether this is the first time opening the websocket, or a reconnection attempt.
+	 * 
+	 * @return websocket
+	 */
 	public WebSocket<JsonNode> index(String user, String userRegion, boolean reconnect) {
 		if (user.isEmpty() || userRegion.isEmpty()) return null;
 		return new NationStatesWebSocket(this.getDatabase(), new DefaultPage(), sanitizeName(user), sanitizeName(userRegion), reconnect);
 	}
 
+	/**
+	 * Opens a websocket for the given user nation and their current home region, when a user is viewing a region page (which may or may not be their own).
+	 * 
+	 * @param user nation that is opening the websocket.
+	 * @param userRegion name of the region the user currently resides in.
+	 * @param region the name of the region that the user currently is looking at.
+	 * @param reconnect whether this is the first time opening the websocket, or a reconnection attempt.
+	 * 
+	 * @return websocket
+	 */
 	public WebSocket<JsonNode> region(String nation, String userRegion, String region, boolean reconnect) {
 		if (nation.isEmpty() || userRegion.isEmpty() || region.isEmpty()) return null;
 		region = sanitizeName(region);
 		return new NationStatesWebSocket(this.getDatabase(), new RegionPage(region, getDatabase().getRegionId(region)), sanitizeName(nation), sanitizeName(userRegion), reconnect);
 	}
 
+
+	/**
+	 * Opens a websocket for the given user nation and their current home region, when a user is viewing a nation page (which may or may not be their own).
+	 * 
+	 * @param user nation that is opening the websocket.
+	 * @param userRegion name of the region the user currently resides in.
+	 * @param visibleNation the name of the nation that the user currently is looking at.
+	 * @param reconnect whether this is the first time opening the websocket, or a reconnection attempt.
+	 * 
+	 * @return websocket
+	 */
 	public WebSocket<JsonNode> nation(String nation, String userRegion, String visibleNation, boolean reconnect) {
 		if (nation.isEmpty() || userRegion.isEmpty() || visibleNation.isEmpty()) return null;
 		visibleNation = sanitizeName(visibleNation);
 		return new NationStatesWebSocket(this.getDatabase(), new NationPage(visibleNation, getDatabase().getNationId(visibleNation)), sanitizeName(nation), sanitizeName(userRegion), reconnect);
 	}
 
+	/**
+	 * Opens a websocket for the given user nation and their current home region, and the region they are viewing the regional controls page for.
+	 * 
+	 * @param user nation that is opening the websocket.
+	 * @param userRegion name of the region the user currently resides in.
+	 * @param adminRegion the name of the region that the user currently is looking at in the regional controls.
+	 * @param reconnect whether this is the first time opening the websocket, or a reconnection attempt.
+	 * 
+	 * @return websocket
+	 */
 	public WebSocket<JsonNode> recruitmentAdmin(String nation, String userRegion, String adminRegion, boolean reconnect) {
 		if (nation.isEmpty() || userRegion.isEmpty() || adminRegion.isEmpty()) return null;
 		return new NationStatesWebSocket(this.getDatabase(), new RecruitmentAdministrationPage(adminRegion, getDatabase().getRegionId(adminRegion)), sanitizeName(nation), sanitizeName(userRegion), reconnect);
